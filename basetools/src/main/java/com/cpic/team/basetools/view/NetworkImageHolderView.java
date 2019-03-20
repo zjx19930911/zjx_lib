8f6739b66a2ad6550b58215ad201657f05df024a
jxzhang10 <jxzhang10@iflytek.com>
2019-03-20 16:28:27 +0800

    改改

--- basetools/src/main/java/com/cpic/team/basetools/view/NetworkImageHolderView.java
+++ basetools/src/main/java/com/cpic/team/basetools/view/NetworkImageHolderView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 
+
 import com.bigkoo.convenientbanner.holder.Holder;
 import com.cpic.team.basetools.R;
 import com.squareup.picasso.Picasso;
@@ -12,18 +13,26 @@ import com.squareup.picasso.Picasso;
 /**
  * Created by Taylor on 2016/8/19.
  */
-public class NetworkImageHolderView implements Holder<String> {
+public class NetworkImageHolderView extends Holder<String> {
     private ImageView imageView;
-    @Override
-    public View createView(Context context) {
-        //你可以通过layout文件来创建，也可以像我一样用代码创建，不一定是Image，任何控件都可以进行翻页
-        imageView = new ImageView(context);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        return imageView;
+    private Context context;
+
+    public NetworkImageHolderView(View itemView) {
+        super(itemView);
+    }
+
+    public NetworkImageHolderView(View itemView, Context context) {
+        super(itemView);
+        this.context = context;
     }
 
     @Override
-    public void UpdateUI(Context context, int position, String data) {
+    protected void initView(View itemView) {
+        imageView = (ImageView) itemView;
+    }
+
+    @Override
+    public void updateUI(String data) {
         imageView.setImageResource(R.drawable.empty_photo2);
         Picasso.with(context)
                 .load(data)
@@ -32,6 +41,5 @@ public class NetworkImageHolderView implements Holder<String> {
                 .centerCrop()
                 .resize(500, 250)
                 .into(imageView);
-//        Glide.with(SampleApplicationContext.context).load(data).placeholder(R.drawable.empty_photo2).error(R.drawable.empty_photo2).into(imageView);
     }
 }
